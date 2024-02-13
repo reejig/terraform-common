a91db379279a6bf4f0b68140eb4a32bf9e16a780
Craig Barr <craig.barr@reejig.com>
2024-02-13 10:15:12 +1000

    feat: eks-iam

---
+++ eks-iam/main.tf
@@ -0,0 +1,17 @@
+data "aws_ssm_parameter" "oidc_provider_arn" {
+  name  = "/eks/${var.cluster_name}/oidc_provider_arn"
+}
+
+module "reejig_app_irsa" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name = "eks-${var.role_name}"
+  role_policy_arns = var.role_policy_arns
+
+  oidc_providers = {
+    main = {
+      provider_arn               = data.aws_ssm_parameter.oidc_provider_arn[0].value
+      namespace_service_accounts = [var.namespace_service_accounts]
+    }
+  }
+}
