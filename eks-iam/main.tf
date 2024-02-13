ff74b348bd43ff8cfe4a69830462b8a5947e9f9e
Craig Barr <craig.barr@reejig.com>
2024-02-14 09:49:57 +1000

    fix

--- eks-iam/main.tf
+++ eks-iam/main.tf
@@ -1,8 +1,3 @@
-resource "aws_iam_policy" "policy" {
-  name        = var.role_name
-  description = "EKS policy for ${var.role_name}"
-  policy      = var.policy
-}
 data "aws_ssm_parameter" "oidc_provider_arn" {
   name  = "/eks/${var.cluster_name}/oidc_provider_arn"
 }
