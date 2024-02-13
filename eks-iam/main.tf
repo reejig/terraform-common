a7e4617dce18a31458118426fcdcb00f476a25c4
m0un10 <7553951+m0un10@users.noreply.github.com>
2024-02-13 10:22:17 +1000

    fix

--- eks-iam/main.tf
+++ eks-iam/main.tf
@@ -1,17 +1,21 @@
+resource "aws_iam_policy" "policy" {
+  name        = var.role_name
+  description = "EKS policy for ${var.role_name}"
+  policy      = var.policy
+}
 data "aws_ssm_parameter" "oidc_provider_arn" {
   name  = "/eks/${var.cluster_name}/oidc_provider_arn"
 }
-
 module "reejig_app_irsa" {
   source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-
   role_name = "eks-${var.role_name}"
-  role_policy_arns = var.role_policy_arns
-
+  role_policy_arns = {
+    policy = aws_iam_policy.policy.arn
+  }
   oidc_providers = {
     main = {
       provider_arn               = data.aws_ssm_parameter.oidc_provider_arn[0].value
-      namespace_service_accounts = [var.namespace_service_accounts]
+      namespace_service_accounts = var.namespace_service_accounts
     }
   }
 }
--- eks-iam/variables.tf
+++ eks-iam/variables.tf
@@ -1,4 +1,4 @@
 variable "cluster_name" {}
 variable "role_name" {}
-variable "role_policy_arns" {}
 variable "namespace_service_accounts" {}
+variable "policy" {}
